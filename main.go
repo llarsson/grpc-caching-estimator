839d9385b1b968af005c5e346e1abfc24f34b50c
Lars Larsson <larsson@cs.umu.se>
2019-12-17 09:49:47 +0100

    Reuse single backend connection

--- main.go
+++ main.go
@@ -1,39 +1,24 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
 	"github.com/mwitkow/grpc-proxy/proxy"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"log"
 	"net"
 	"os"
+	"strconv"
 )
 
 var (
 	director proxy.StreamDirector
 )
 
-func CacheEstimatingStreamDirector(ctx context.Context, fullMethodName string) (context.Context, *grpc.ClientConn, error) {
-	_, ok := metadata.FromIncomingContext(ctx)
-	if ok {
-		conn, err := grpc.DialContext(ctx,
-			os.Getenv("PROXY_UPSTREAM_HOST")+":"+os.Getenv("PROXY_UPSTREAM_PORT"),
-			grpc.WithCodec(proxy.Codec()),
-			grpc.WithInsecure())
-
-		EstimateMaxAge(ctx, fullMethodName)
-
-		return ctx, conn, err
-	}
-
-	return nil, nil, grpc.Errorf(codes.Unimplemented, "Unknown method")
-}
-
-func EstimateMaxAge(ctx context.Context, fullMethodName string) {
+func EstimateMaxAge(ctx context.Context, fullMethodName string) (int, error) {
 	md, _ := metadata.FromIncomingContext(ctx)
 
 	log.Println(fmt.Sprintf("%s :: %s", fullMethodName, md))
@@ -43,24 +28,42 @@ func EstimateMaxAge(ctx context.Context, fullMethodName string) {
 		{
 			maxAge, err := estimatedValidity(ctx, fullMethodName)
 			if err != nil {
-				panic(err)
+				return -1, err
 			}
-			grpc.SetHeader(ctx, metadata.Pairs("cache-control", "must-revalidate, max-age="+maxAge))
+			grpc.SetHeader(ctx, metadata.Pairs("cache-control", fmt.Sprintf("must-revalidate, max-age=%d", maxAge)))
+			return maxAge, nil
 		}
 	case "passthrough":
 		{
+			return -1, nil
 		}
 	default:
-		grpc.SetHeader(ctx, metadata.Pairs("cache-control", "must-revalidate, max-age="+os.Getenv("PROXY_MAX_AGE")))
+		maxAge, err := strconv.Atoi(os.Getenv("PROXY_MAX_AGE"))
+		if err != nil {
+			log.Printf("Failed to parse PROXY_MAX_AGE (%s) into integer", os.Getenv("PROXY_MAX_AGE"))
+			return -1, err
+		}
+		grpc.SetHeader(ctx, metadata.Pairs("cache-control", fmt.Sprintf("must-revalidate, max-age=%d", maxAge)))
+		return maxAge, nil
 	}
 }
 
-func estimatedValidity(ctx context.Context, fullMethodName string) (string, error) {
-	return "", errors.New("Not implemented yet")
+func estimatedValidity(ctx context.Context, fullMethodName string) (int, error) {
+	return -1, status.Errorf(codes.Unimplemented, "Dynamic validity not implemented yet")
 }
 
 func main() {
-	director = CacheEstimatingStreamDirector
+	upstream := fmt.Sprintf("%s:%s", os.Getenv("PROXY_UPSTREAM_HOST"), os.Getenv("PROXY_UPSTREAM_PORT"))
+	conn, err := grpc.Dial(upstream, grpc.WithCodec(proxy.Codec()), grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Failed to connect to upstream service at %s", upstream)
+	}
+
+	director = func(ctx context.Context, fullMethodName string) (context.Context, *grpc.ClientConn, error) {
+		_, err = EstimateMaxAge(ctx, fullMethodName)
+		return ctx, conn, err
+	}
 
 	server := grpc.NewServer(grpc.CustomCodec(proxy.Codec()), grpc.UnknownServiceHandler(proxy.TransparentHandler(director)))
 
